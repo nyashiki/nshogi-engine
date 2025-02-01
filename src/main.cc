1d3f67b4078b6929f1ff6221a814308dfab6947c
nyashiki <tnakayashiki01@gmail.com>
2025-02-02 01:19:19 +0900

    Delete: unused codes

--- src/main.cc
+++ src/main.cc
@@ -9,30 +9,12 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
-#include <random>
-#include "allocator/allocator.h"
-#include "allocator/fixed_allocator.h"
-#include "allocator/segregated_free_list.h"
-#include "mcts/node.h"
-#include "mcts/edge.h"
 
-#include <cstdio>
-#include <cinttypes>
 #include "protocol/usi.h"
 
 #include <nshogi/core/initializer.h>
 
 int main() {
-    // nshogi::engine::allocator::SegregatedFreeListAllocator<128> Alloc;
-    // // nshogi::engine::allocator::FixedAllocator<8> Alloc;
-    // Alloc.resize(1024);
-    // void* Ptr1 = Alloc.malloc(10);
-    // void* Ptr2 = Alloc.malloc(10);
-    // Alloc.free(Ptr1);
-    // Alloc.free(Ptr2);
-    // return 0;
-
     nshogi::core::initializer::initializeAll();
 
     std::string Command;
