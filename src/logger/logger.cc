caa027843b5792373f898728d924cdd307a0d935
nyashiki <tnakayashiki01@gmail.com>
2025-04-04 23:39:46 +0900

    Add: missing logger.cc file

---
+++ src/logger/logger.cc
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2025 @nyashiki
+//
+// This software is licensed under the MIT license.
+// For details, see the LICENSE file in the root of this repository.
+//
+// SPDX-License-Identifier: MIT
+//
+
+#include "logger.h"
+
+namespace nshogi {
+namespace engine {
+namespace logger {
+
+void Logger::setIsNShogiExtensionLogEnabled(bool Value) {
+    IsNShogiExtensionEnabled = Value;
+}
+
+} // namespace logger
+} // namespace engine
+} // namespace logger
