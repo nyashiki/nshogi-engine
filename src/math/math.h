ec31c625189fe4f756f7ea7b31beb43994326e22
nyashiki <tnakayashiki01@gmail.com>
2025-05-11 14:52:41 +0900

    Add: float version of nan check

--- src/math/math.h
+++ src/math/math.h
@@ -20,16 +20,20 @@ namespace math {
 // Since std::isnan is not available when -ffast-math is specified,
 // we manually check for NaN using bitwise comparison based on the IEEE 754
 // standard.
-inline bool isnan_(double X) {
-    union {
-        double D;
-        uint64_t I;
-    } U;
-    U.D = X;
-    const uint64_t ExponentMask = 0x7FF0000000000000ULL;
-    const uint64_t FractionMask = 0x000FFFFFFFFFFFFFULL;
-    return ((U.I & ExponentMask) == ExponentMask) &&
-           ((U.I & FractionMask) != 0);
+template<std::same_as<double> T>
+inline bool isnan_(T X) {
+    const auto Bits = std::bit_cast<std::uint64_t>(X);
+    constexpr uint64_t ExponentMask = 0x7FF0'0000'0000'0000ULL;
+    constexpr uint64_t FractionMask = 0x000F'FFFF'FFFF'FFFFULL;
+    return ((Bits & ExponentMask) == ExponentMask) && ((Bits & FractionMask) != 0);
+}
+
+template<std::same_as<float> T>
+inline bool isnan_(T X) {
+    const auto Bits = std::bit_cast<std::uint32_t>(X);
+    constexpr uint32_t ExponentMask = 0x7F80'0000U;
+    constexpr uint32_t FractionMask = 0x007F'FFFFU;
+    return ((Bits & ExponentMask) == ExponentMask) && ((Bits & FractionMask) != 0);
 }
 
 inline float sigmoid(float X) {
